0e4d4769a35852ca7d3035d96b82da2bbbda3661
keliiiiw <majutakgentar291@gmail.com>
2026-06-07 20:54:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,12 +85,12 @@ st.markdown(tema_pink, unsafe_allow_html=True)
 
 
 # --- KONTEN WEB ---
-st.markdown("<h1>Halo Sayang... 🎀</h1>", unsafe_allow_html=True)
+st.markdown("<h1>Hai Andrea.. 🎀</h1>", unsafe_allow_html=True)
 st.write("---")
 
-st.write("Aku tau aku salah, dan aku bener-bener minta maaf ya.")
-st.write("Website kecil ini aku buat khusus buat kamu, sebagai tanda kalau aku serius mau perbaikin semuanya.")
-st.write("Aku sayang banget sama kamu.")
+st.write("Aku bener bener salah, dan aku bener-bener minta maaf ya.")
+st.write("Website kecil ini aku buat khusus buat kamu, emang sepele dan bener bener freak banget")
+st.write("Tapi aku harap kamu bener bener happy karena gebrakan aneh aku ini ya")
 
 st.write("") # Spasi kosong biar rapi
 
@@ -109,7 +109,7 @@ with col2:
 
 if tombol_maaf:
     st.balloons()
-    st.toast("Terima kasih sayang... ❤️", icon="💖")
+    st.toast("Aku sayang kamu.. ❤️", icon="💖")
     
     pesan_sukses = (
         "<div style='padding: 30px; background-color: #FCE4EC; border-radius: 15px; color: #5E3A41; margin-top: 25px; border: 1px solid #F8BBD0; box-shadow: 0px 4px 10px rgba(0,0,0,0.05);'>"
@@ -140,12 +140,12 @@ if tombol_maaf:
     st.markdown(pesan_sukses, unsafe_allow_html=True)
 
 elif tombol_bete:
-    st.toast("Gapapa, aku ngerti... 🥺", icon="🌧️")
+    st.toast("isokey, aku ngerti tapi jangan lama lama ya cantik, aku kangen 🥺", icon="🌧️")
     
     pesan_error = (
         "<div style='text-align: center; padding: 20px; background-color: #F5EAEB; border-radius: 15px; color: #5E3A41; margin-top: 25px; border: 1px solid #E2A9B4; box-shadow: 0px 4px 10px rgba(0,0,0,0.05);'>"
-        "<b>Gapapa kalau kamu masih marah.</b><br>"
-        "Aku bakal terus usaha sampe kamu luluh. 😔🫶"
+        "<b>aku kangen banget andrea.. jangan lama lama ya.. </b><br>"
+        "Aku kangen banget sama kamu, bener bener sekangen itu.. 😔🫶"
         "</div>"
     )
     st.markdown(pesan_error, unsafe_allow_html=True)
